9cf79d039330b00ae8994b64c8595c9aa4d1806d
Fred <36563@ma-web.nl>
2023-11-24 11:54:53 +0100

    opdracht 02 gemaakt

---
+++ 02/app.js
@@ -0,0 +1,16 @@
+const gamestate_start=0;
+const gamestate_ingame=1;
+const gamestate_gameover=2;
+const ingamestate_start=0;
+const ingamestate_roll=1;
+const ingamestate_end=0;
+
+let boardPositionSize= 50;
+let pawnPositions = [];
+let boardPositions=[];
+let playerAmountButtons = [];
+
+
+let canvas = document.getElementById("canvas");
+let g = canvas.getContext("2d");
+console.log(g)
